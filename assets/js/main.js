f99b4cba73a23d86cfbdd144e6b7f0b2255c3cdd
MKElyes <elyesmkaddem@gmail.com>
2019-05-16 10:37:22 +0100

    six commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,18 +25,28 @@ $(document).ready(function() {
 
 
 
-         var tab = [];
+      //    var tab = [];
 
-         var obj = {
-           'url': "(assets/img/categ_make_up_home@2x.png)",
-           'cardT': "elyes",
-           'cardDes': 'elyes elyes'
+      //    var obj = {
+      //      'url': "assets/img/categ_make_up_home@2x.png",
+      //      'cardT': "elyes",
+      //      'cardDes': 'elyes elyes'
            
-         }
-         tab.push(obj);
-         for (i=0; i<tab.length; i++){
+      //    }
+      //    tab.push(obj);
+      //    for (i=0; i<tab.length; i++){
        
-          card = ' <div class="carousel-item col-md-4"> <div class="card"> <img class="card-img-top img-fluid" src="assets/img/categ_lashes_home@1x.png" alt="Card image cap"> <div class="card-body"> <h4 class="card-title">'+tab[i].cardT+'</h4> <p class="card-text text-center">'+tab[i].cardDes+'</p> </div> </div> </div>';
-          document.getElementById('show').innerHTML+= card;
-       }
+      //     card = ' <div class="carousel-item col-md-4"> <div class="card"> <img class="card-img-top img-fluid" src="'+tab[i].url+'" alt="Card image cap"> <div class="card-body"> <h4 class="card-title">'+tab[i].cardT+'</h4> <p class="card-text text-center">'+tab[i].cardDes+'</p> </div> </div> </div>';
+      //     document.getElementById('show').innerHTML+= card;
+      //  }
 
+       $.getJSON('https://jsonp.afeld.me/?url=https%3A%2F%2Fapi.myglamapp.pl%2Fapi%2Fcategories%3Flanguage%3DEN', function(data) {
+         var text = data.data;
+     console.log(text)
+         for (i=0; i<text.length; i++) {
+          card = ' <div class="carousel-item col-md-4"> <div class="card"> <img class="card-img-top img-fluid" src="http://'+text[i].imagePath+'" alt="Card image cap"> <div class="card-body"> <h4 class="card-title">'+text[i].label+'</h4> <p class="card-text text-center">'+text[i].description+'</p> </div> </div> </div>';
+          document.getElementById('cardshow').innerHTML+= card;
+         }
+         
+          });
+       
